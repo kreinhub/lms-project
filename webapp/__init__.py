40be22909f3551776e206c9dcc33c784ee22331e
krein <sirionhvg@gmail.com>
2019-10-06 12:55:59 +0300

    Do some correctives in init and create create_db

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,22 +1,18 @@
 from flask import Flask, render_template
 
-from model import db, Users, Content, Progress
+from webapp.model import db, Users, Content, Progress
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-
+    db.init_app(app)
 
     @app.route('/')
     def index():
         return render_template('index.html')
 
     return app
-
-
-def create_db(app, db):
-    pass
 
 
 def save_data(db, *args):       # *args are table names. perhaps using *kwargs will be better
