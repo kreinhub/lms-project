fa5897b0bc681b2c6a9add15bf41de0310dea63f
krein <sirionhvg@gmail.com>
2019-10-06 12:40:23 +0300

    Add dependencies in requirements, create config.py, move model to webapp/ and do some changes in it

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -5,6 +5,8 @@ from model import db, Users, Content, Progress
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+
 
     @app.route('/')
     def index():
--- webapp/model.py
+++ webapp/model.py
@@ -9,13 +9,13 @@ db = SQLAlchemy()
 
 class Users(db.Model, UserMixin):
     user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
-    password = db.Column(db.String(120))
-    email = db.Column(db.String(120))
-    role = db.Column(db.String(50))
-    gender = db.Column(db.String(50))
-    age = db.Column(db.Integer)
-    phone = db.Column(db.String(50))
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(50), default="student")
+    gender = db.Column(db.String(50), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    phone = db.Column(db.String(50), nullable=True)
     last_login = db.Column(db.DateTime, default=datetime.datetime.now)
     registered = db.Column(db.DateTime, default=datetime.datetime.now)
 
@@ -26,23 +26,27 @@ class Users(db.Model, UserMixin):
         return check_password_hash(self.password, password)
 
     def __repr__(self):
-        return '<User {}>'.format(self.email)
+        return f'<User {self.username} {self.email}>'
 
 
 class Content(db.Model):
     theme = db.Column(db.String(120), primary_key=True)
-    section = db.Column(db.String(50))
-    description = db.Column(db.String(120))
-    type_ = db.Column(db.String(50))
-    url = db.Column(db.String(120))
+    section = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(120), nullable=True)
+    type_ = db.Column(db.String(50), nullable=False)
+    url = db.Column(db.String(120), nullable=False)
     create_date = db.Column(db.DateTime, default=datetime.datetime.now)
     modified_date = db.Column(db.DateTime, default=datetime.datetime.now)
-    rating = db.Column(db.Integer)
+    rating = db.Column(db.Integer)      # need to specify default arg
 
+    def __repr__(self):
+        return f'<Content {self.theme} {self.type_}>'
 
 class Progress(db.Model, UserMixin):
     record_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    theme = db.Column(db.Integer, db.ForeignKey('content.theme'))
+    theme = db.Column(db.Integer, db.ForeignKey('content.theme')) # Integer?
+    section = db.Column(db.String, db.ForeignKey('content.section'))
 
-
+    def __repr__(self):
+        return f'<Users progress by {self.theme}>'
